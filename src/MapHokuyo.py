6626864d54ef517438c48c95eb94dc1843be05ba
juanpablo920 <2420171051@estudiantesunibague.edu.co>
2022-06-02 18:53:44 -0500

    a

--- src/MapHokuyo.py
+++ src/MapHokuyo.py
@@ -3,6 +3,7 @@
 import rospy
 import rospkg
 import tf
+import numpy as np
 from std_msgs.msg import Int16
 from sensor_msgs.msg import MultiEchoLaserScan
 from sensor_msgs.msg import PointCloud2
@@ -25,10 +26,14 @@ class MapHokuyo:
         self.sub_lidar = None
         self.sub_motor = None
 
-        self.pub_map = None
+        self.pub_map_echo1 = None
+        self.pub_map_echo2 = None
+        self.pub_map_echo3 = None
 
         self.pub_lidar_flag = None
         self.pub_motor_flag = None
+
+        self.flag_Echo = False
 
     def setting_subscriber(self):
         self.sub_lidar = rospy.Subscriber(
@@ -44,8 +49,16 @@ class MapHokuyo:
             queue_size=100)
 
     def setting_publisher(self):
-        self.pub_map = rospy.Publisher(
-            "/map_hokuyo/map/pointcloud",
+        self.pub_map_echo1 = rospy.Publisher(
+            "/map_hokuyo/echo1/pointcloud",
+            PointCloud2)
+
+        self.pub_map_echo2 = rospy.Publisher(
+            "/map_hokuyo/echo2/pointcloud",
+            PointCloud2)
+
+        self.pub_map_echo3 = rospy.Publisher(
+            "/map_hokuyo/echo3/pointcloud",
             PointCloud2)
 
         self.pub_lidar_flag = rospy.Publisher(
@@ -67,14 +80,51 @@ class MapHokuyo:
         while self.flag_Echo == False:
             pass
         self.flag_Echo = False
+        multiEcho_ros = self.multiEcho_ros
 
-        int_Lidar_ros = self.multiEcho_ros.intensities
-        print(int_Lidar_ros)
-        print(type(int_Lidar_ros))
-        range_Lidar_ros = self.multiEcho_ros.ranges
-        print(range_Lidar_ros)
-        print(type(range_Lidar_ros))
-        print(range_Lidar_ros)
+        angle_min = multiEcho_ros.angle_min
+        angle_max = multiEcho_ros.angle_max
+        range_Lidar_ros = multiEcho_ros.ranges
+        int_Lidar_ros = multiEcho_ros.intensities
+
+        N = len(range_Lidar_ros)
+        tetha = np.linspace(angle_min, angle_max, N)
+
+        ranges_0 = np.zeros([1, N])
+        ranges_1 = np.zeros([1, N])
+        ranges_2 = np.zeros([1, N])
+        intensities_0 = np.zeros([1, N])
+        intensities_1 = np.zeros([1, N])
+        intensities_2 = np.zeros([1, N])
+
+        for idx in range(0, N):
+            range_tmp = range_Lidar_ros[idx]
+            intensities_tmp = int_Lidar_ros[idx]
+            print(len(range_tmp.echoes))
+            print(len(intensities_tmp.echoes))
+            print("-"*10)
+            N_tmp = len(intensities_tmp.echoes)
+
+            ranges_0[0, idx] = range_tmp.echoes[0]
+            intensities_0[0, idx] = intensities_tmp.echoes[0]
+
+            if N_tmp > 1:
+                ranges_1[0, idx] = range_tmp.echoes[1]
+                intensities_1[0, idx] = intensities_tmp.echoes[1]
+                if N_tmp > 2:
+                    ranges_2[0, idx] = range_tmp.echoes[2]
+                    intensities_2[0, idx] = intensities_tmp.echoes[2]
+
+            
+
+        # # print(range_Lidar_ros)
+        # print(type(range_Lidar_ros[0]))
+        # # print(len(range_Lidar_ros[0]))
+        # print(range_Lidar_ros)
+
+        # # print(int_Lidar_ros)
+        # print(type(int_Lidar_ros))
+        # print(len(int_Lidar_ros))
 
         self.pub_motor_flag.publish(0)
 
