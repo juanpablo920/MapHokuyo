405297fb62838ae97d463206e8422d1f5b2d531b
sirui1 <2420171062@estudiantesunibague.edu.co>
2022-06-02 19:49:53 -0500

    ac

--- src/MapHokuyo.py
+++ src/MapHokuyo.py
@@ -5,6 +5,7 @@ import rospkg
 import tf
 import numpy as np
 from std_msgs.msg import Int16
+from sensor_msgs import point_cloud2
 from sensor_msgs.msg import MultiEchoLaserScan
 from sensor_msgs.msg import PointCloud2
 from geometry_msgs.msg import TransformStamped
@@ -26,14 +27,14 @@ class MapHokuyo:
         self.sub_lidar = None
         self.sub_motor = None
 
+        self.pub_map_echo0 = None
         self.pub_map_echo1 = None
         self.pub_map_echo2 = None
-        self.pub_map_echo3 = None
+
+        self.flag_Echo = False
 
         self.pub_lidar_flag = None
         self.pub_motor_flag = None
-
-        self.flag_Echo = False
 
     def setting_subscriber(self):
         self.sub_lidar = rospy.Subscriber(
@@ -49,25 +50,30 @@ class MapHokuyo:
             queue_size=100)
 
     def setting_publisher(self):
+        self.pub_map_echo0 = rospy.Publisher(
+            "/map_hokuyo/echo0/pointcloud",
+            PointCloud2,
+            queue_size=10)
+
         self.pub_map_echo1 = rospy.Publisher(
             "/map_hokuyo/echo1/pointcloud",
-            PointCloud2)
+            PointCloud2,
+            queue_size=10)
 
         self.pub_map_echo2 = rospy.Publisher(
             "/map_hokuyo/echo2/pointcloud",
-            PointCloud2)
-
-        self.pub_map_echo3 = rospy.Publisher(
-            "/map_hokuyo/echo3/pointcloud",
-            PointCloud2)
+            PointCloud2,
+            queue_size=10)
 
         self.pub_lidar_flag = rospy.Publisher(
             "map_hokuyo/lidar/flag",
-            Int16)
+            Int16,
+            queue_size=1)
 
         self.pub_motor_flag = rospy.Publisher(
             "map_hokuyo/motor/flag",
-            Int16)
+            Int16,
+            queue_size=1)
 
     def scanning(self, multiEcho_ros):
         self.pub_lidar_flag.publish(1)
@@ -81,6 +87,8 @@ class MapHokuyo:
             pass
         self.flag_Echo = False
         multiEcho_ros = self.multiEcho_ros
+
+        header_echos = multiEcho_ros.header
 
         angle_min = multiEcho_ros.angle_min
         angle_max = multiEcho_ros.angle_max
@@ -100,9 +108,6 @@ class MapHokuyo:
         for idx in range(0, N):
             range_tmp = range_Lidar_ros[idx]
             intensities_tmp = int_Lidar_ros[idx]
-            print(len(range_tmp.echoes))
-            print(len(intensities_tmp.echoes))
-            print("-"*10)
             N_tmp = len(intensities_tmp.echoes)
 
             ranges_0[0, idx] = range_tmp.echoes[0]
@@ -115,21 +120,31 @@ class MapHokuyo:
                     ranges_2[0, idx] = range_tmp.echoes[2]
                     intensities_2[0, idx] = intensities_tmp.echoes[2]
 
-            
+        pcd_echo0 = np.ones([3, N])
+        pcd_echo0[0, :] = ranges_0*np.cos(tetha)
+        pcd_echo0[1, :] = ranges_0*np.sin(tetha)
+        pcd_echo0[2, :] = np.zeros([1, N])
 
-        # # print(range_Lidar_ros)
-        # print(type(range_Lidar_ros[0]))
-        # # print(len(range_Lidar_ros[0]))
-        # print(range_Lidar_ros)
+        pcd_echo1 = np.ones([3, N])
+        pcd_echo1[0, :] = ranges_1*np.cos(tetha)
+        pcd_echo1[1, :] = ranges_1*np.sin(tetha)
+        pcd_echo1[2, :] = np.zeros([1, N])
 
-        # # print(int_Lidar_ros)
-        # print(type(int_Lidar_ros))
-        # print(len(int_Lidar_ros))
+        pcd_echo2 = np.ones([3, N])
+        pcd_echo2[0, :] = ranges_2*np.cos(tetha)
+        pcd_echo2[1, :] = ranges_2*np.sin(tetha)
+        pcd_echo2[2, :] = np.zeros([1, N])
+
+        self.pub_map_echo0.publish(
+            point_cloud2.create_cloud_xyz32(header_echos, pcd_echo0.T))
+
+        self.pub_map_echo1.publish(
+            point_cloud2.create_cloud_xyz32(header_echos, pcd_echo1.T))
+
+        self.pub_map_echo2.publish(
+            point_cloud2.create_cloud_xyz32(header_echos, pcd_echo2.T))
 
         self.pub_motor_flag.publish(0)
-
-    def publish_flags():
-        pass
 
 
 if __name__ == '__main__':
